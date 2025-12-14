1d90984729c5fca9b1349212dc8b3b80b04cb569
秦浩楠 <22377272>
2025-12-14 18:52:13 +0800

    Revert "add mock for test"
    This reverts commit 1c2fc200b144140eea0da4270e123bdc6ff734a9.

--- src/main.ts
+++ src/main.ts
@@ -12,14 +12,6 @@ import '@mdi/font/css/materialdesignicons.css'
 import App from './App.vue'
 import router from './router'
 
-// 初始化 MSW (仅在开发环境)
-if (import.meta.env.DEV) {
-  const { worker } = await import('./mocks/worker')
-  await worker.start({
-    onUnhandledRequest: 'bypass',
-  })
-}
-
 const vuetify = createVuetify({
   components,
   directives,
